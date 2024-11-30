a65e8c335f45182b6f64883c3eaa86a37507cc80
JoksLobo2 <jocsonherry@gmail.com>
2024-11-30 08:12:45 -0300

    criação de modal

--- src/components/MenuList/index.tsx
+++ src/components/MenuList/index.tsx
@@ -1,25 +1,72 @@
+import { useState } from 'react'
+import { FoodList, Modal, ModalContent, ModalImg } from './styles'
+
 import Menu from '../Menu'
 import Food from '../../Models/Food'
-import { FoodList } from './styles'
+import pizza from '../../assets/images/pizza.png'
+import close from '../../assets/images/close 1.png'
 
 type Props = {
   title: string
   foods: Food[]
 }
 
-const MenuList = ({ foods }: Props) => (
-  <div className="container">
-    <FoodList>
-      {foods.map((food) => (
-        <Menu
-          key={food.id}
-          title={food.title}
-          description={food.description}
-          image={food.image}
-        />
-      ))}
-    </FoodList>
-  </div>
-)
+const MenuList = ({ foods }: Props) => {
+  const [modalEstaAberta, setModalEstaAberta] = useState(false)
+
+  return (
+    <>
+      <div className="container">
+        <FoodList>
+          {foods.map((food) => (
+            <Menu
+              key={food.id}
+              title={food.title}
+              description={food.description}
+              image={food.image}
+            />
+          ))}
+        </FoodList>
+      </div>
+      <Modal className={modalEstaAberta ? 'visible' : ''}>
+        <ModalContent className="container">
+          <div>
+            <ModalImg src={pizza} alt="" />
+            <div>
+              <div>
+                <h2>Pizza Marguerita</h2>
+                <img
+                  src={close}
+                  alt=""
+                  onClick={() => setModalEstaAberta(false)}
+                />
+              </div>
+              <p>
+                A pizza Margherita é uma pizza clássica da culinária italiana,
+                reconhecida por sua simplicidade e sabor inigualável. Ela é
+                feita com uma base de massa fina e crocante, coberta com molho
+                de tomate fresco, queijo mussarela de alta qualidade, manjericão
+                fresco e azeite de oliva extra-virgem. A combinação de sabores é
+                perfeita, com o molho de tomate suculento e ligeiramente ácido,
+                o queijo derretido e cremoso e as folhas de manjericão frescas,
+                que adicionam um toque de sabor herbáceo. É uma pizza simples,
+                mas deliciosa, que agrada a todos os paladares e é uma ótima
+                opção para qualquer ocasião.
+              </p>
+              <p>Serve: de 2 a 3 pessoas</p>
+              <button type="button" onClick={() => setModalEstaAberta(true)}>
+                Adcionar ao carrinho - R$60,90
+              </button>
+            </div>
+          </div>
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => setModalEstaAberta(false)}
+        ></div>
+      </Modal>
+    </>
+  )
+}
 
 export default MenuList
--- src/components/MenuList/styles.ts
+++ src/components/MenuList/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { cores } from '../../styles'
 
 export const FoodList = styled.ul`
   width: 100%;
@@ -7,4 +8,90 @@ export const FoodList = styled.ul`
   gap: 32px;
   margin-top: 56px;
   margin-bottom: 122px;
+`
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: none;
+  justify-content: center;
+  align-items: center;
+
+  &.visible {
+    display: flex;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.73);
+  }
+`
+export const ModalContent = styled.div`
+  max-width: 1024px;
+  position: relative;
+  z-index: 1;
+
+  > div {
+    display: flex;
+    padding: 0 0 32px 32px;
+    background-color: ${cores.rosa};
+    color: ${cores.branco};
+
+    > img {
+      width: 280px;
+      height: 280px;
+      margin-right: 24px;
+      object-fit: cover;
+      margin-top: 32px;
+    }
+
+    div {
+      display: block;
+
+      div {
+        display: flex;
+        justify-content: space-between;
+
+        h2 {
+          font-size: 21px;
+          font-weight: 900;
+          margin-top: 32px;
+        }
+
+        img {
+          height: 16px;
+          width: 16px;
+          margin: 8px;
+          cursor: pointer;
+        }
+      }
+
+      p {
+        margin: 16px 0;
+        font-size: 14px;
+      }
+
+      button {
+        border: none;
+        padding: 4px 7px;
+        font-size: 14px;
+        font-weight: 700;
+        background-color: ${cores.branco};
+        color: ${cores.rosa};
+        cursor: pointer;
+      }
+    }
+  }
+`
+export const ModalImg = styled.img`
+  height: 280px;
+  width: 280px;
+  object-fit: cover;
 `
