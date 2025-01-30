b276489d5af786e8619ffc64accec2c8c4cf5db0
JoksLobo <jocson_herry@hotmail.com>
2025-01-30 15:22:36 -0300

    validação dos campos de payment com formik e yup

--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -12,6 +12,8 @@ import { formataPreco } from '../MenuL'
 import { RootReducer } from '../../store'
 import { closeP } from '../../store/reducers/payment'
 import { openCheck } from '../../store/reducers/checkout'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const Payment = () => {
   const { isOpen } = useSelector((state: RootReducer) => state.payment)
@@ -26,47 +28,134 @@ const Payment = () => {
     dispatch(openCheck())
   }
 
+  const form = useFormik({
+    initialValues: {
+      name: '',
+      cardnumber: '',
+      safetycode: '',
+      expiremonth: '',
+      expireyear: ''
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(5, 'O nome precisa de no mínimo 5 caracteres')
+        .required('Campo obrigatório'),
+      cardnumber: Yup.string().required('Campo obrigatório'),
+      safetycode: Yup.string()
+        .min(3, 'O código de segurança deve ter 3 caracteres')
+        .max(3, 'O código de segurança só pode ter 3 caracteres')
+        .required('Campo obrigatório'),
+      expiremonth: Yup.string().required('Campo obrigatório'),
+      expireyear: Yup.string().required('Campo onrigatório')
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  const getErrorMessage = (fieldName: string, message?: string) => {
+    const estaAlterado = fieldName in form.touched
+    const estaInvalido = fieldName in form.errors
+
+    if (estaAlterado && estaInvalido) return message
+    return ''
+  }
+
   return (
-    <PaymentContainer className={isOpen ? 'is-open' : ''}>
-      <Overlay onClick={closePayment} />
-      <PaymentCard>
-        <InputGroup>
-          <ul>
-            <h2>Pagamento - Valor a pagar R$130,00</h2>
-            <li>
-              <label htmlFor="name">Nome no cartão</label>
-              <input id="name" type="text" />
-            </li>
-            <CardInput>
+    <form onSubmit={form.handleSubmit}>
+      <PaymentContainer className={isOpen ? 'is-open' : ''}>
+        <Overlay onClick={closePayment} />
+        <PaymentCard>
+          <InputGroup>
+            <ul>
+              <h2>Pagamento - Valor a pagar R$130,00</h2>
               <li>
-                <label htmlFor="cardnumber">Número do cartão</label>
-                <input className="card-number" id="cardnumber" type="number" />
+                <label htmlFor="name">Nome no cartão</label>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  value={form.values.name}
+                  onChange={form.handleChange}
+                  onBlur={form.handleBlur}
+                />
+                <small>{(getErrorMessage('name'), form.errors.name)}</small>
               </li>
-              <li>
-                <label htmlFor="safetycode">CVV</label>
-                <input className="cvv" id="safetycode" type="number" />
-              </li>
-            </CardInput>
-            <CardInput>
-              <li>
-                <label htmlFor="expiremonth">Mês de vencimento</label>
-                <input className="expires" id="expiremonth" type="number" />
-              </li>
-              <li>
-                <label htmlFor="expireyear">CVV</label>
-                <input className="expires" id="expireyear" type="number" />
-              </li>
-            </CardInput>
-          </ul>
-        </InputGroup>
-        <PaymentButton className="firstButton">
-          Finalizar pagamento
-        </PaymentButton>
-        <PaymentButton onClick={closePaymentOpenCheckout}>
-          Voltar para edição de endereço
-        </PaymentButton>
-      </PaymentCard>
-    </PaymentContainer>
+              <CardInput>
+                <li>
+                  <label htmlFor="cardnumber">Número do cartão</label>
+                  <input
+                    className="card-number"
+                    id="cardnumber"
+                    type="number"
+                    name="cardnumber"
+                    value={form.values.cardnumber}
+                    onChange={form.handleChange}
+                    onBlur={form.handleBlur}
+                  />
+                  <small>
+                    {(getErrorMessage('cardnumber'), form.errors.cardnumber)}
+                  </small>
+                </li>
+                <li>
+                  <label htmlFor="safetycode">CVV</label>
+                  <input
+                    className="cvv"
+                    id="safetycode"
+                    type="number"
+                    name="safetycode"
+                    value={form.values.safetycode}
+                    onChange={form.handleChange}
+                    onBlur={form.handleBlur}
+                  />
+                  <small>
+                    {(getErrorMessage('safetycode'), form.errors.safetycode)}
+                  </small>
+                </li>
+              </CardInput>
+              <CardInput>
+                <li>
+                  <label htmlFor="expiremonth">Mês de vencimento</label>
+                  <input
+                    className="expires"
+                    id="expiremonth"
+                    type="number"
+                    name="expiremonth"
+                    value={form.values.expiremonth}
+                    onChange={form.handleChange}
+                    onBlur={form.handleBlur}
+                  />
+                  <small>
+                    {(getErrorMessage('expiremonth'), form.errors.expiremonth)}
+                  </small>
+                </li>
+                <li>
+                  <label htmlFor="expireyear">Ano de vencimento</label>
+                  <input
+                    className="expires"
+                    id="expireyear"
+                    type="number"
+                    name="expireyear"
+                    value={form.values.expireyear}
+                    onChange={form.handleChange}
+                    onBlur={form.handleBlur}
+                  />
+                  <small>
+                    {(getErrorMessage('expireyear'), form.errors.expireyear)}
+                  </small>
+                </li>
+              </CardInput>
+            </ul>
+          </InputGroup>
+          <PaymentButton className="firstButton">
+            Finalizar pagamento
+          </PaymentButton>
+          <PaymentButton onClick={closePaymentOpenCheckout}>
+            Voltar para edição de endereço
+          </PaymentButton>
+        </PaymentCard>
+      </PaymentContainer>
+    </form>
   )
 }
 
